016a9b166db4242d67ad749940c5a7a321855521
seherrr <86481287+seherrr@users.noreply.github.com>
2023-03-03 20:45:11 +0300

    Add files via upload

---
+++ dvdrental-Odev5.sql
@@ -0,0 +1,8 @@
+--ODEV5--
+
+--1
+select * from film where title like '%n' order by length desc limit 5;
+--2
+select * from film where title like '%n' order by length offset 5 limit 5;
+--3
+select last_name from customer where store_id=1 order by last_name desc limit 4;
